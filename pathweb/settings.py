c023baf193ff30ec8ad1fae41f67264df9076d3f
joshuagatizz <57672096+joshuagatizz@users.noreply.github.com>
2023-06-09 17:47:22 +0700

    added static root

--- pathweb/settings.py
+++ pathweb/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = "/pathapp/static"
+STATIC_ROOT = '/pathapp/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
